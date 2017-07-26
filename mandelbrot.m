f2046ab00894130a959e342ade767895de97df2b
Abhranil Das <abhranil.das@utexas.edu>
2017-07-26 01:12:28 -0500

    Fixed code.

--- mandelbrot.m
+++ mandelbrot.m
@@ -1,39 +1,46 @@
-%%%%%%%%%%% Mandelbrot Set %%%%%%%%%%%%
-clear all 
+% Mandelbrot set explorer
+clear variables
+tic
 
-% Input parameters
-resolution=[1920 1080];
-center=0;
-width=4;
+% input parameters:
+resolution=[1920 1080]; % in pixels
+center=0;               % physical location of frame center
+width=4;                % frame width in physical units
+max_depth=100;          % max # of iterations
 
-max_depth=10;                   % max number of iterations
-
-%%%%%%% generate grid %%%%%%%%
+% get frame boundaries:
+height=width*resolution(2)/resolution(1);
 left=real(center)-width/2;
 right=real(center)+width/2;
-
-height=width*resolution(2)/resolution(1);
 top=imag(center)+height/2;
 bottom=imag(center)-height/2;
 
+% generate complex grid coordinates:
 [X,Y]=meshgrid(left:width/(resolution(1)-1):right,top:-height/(resolution(2)-1):bottom); 
-C = X + i*Y;
+C=X+1i*Y;
 
-z=zeros(size(C));                    % initial z matrix is complex value matrix
-depth=zeros(size(C)); 
-%%%%%%% apply formula to matrices %%%%%%%%%
+% initiate depth, index, location and value arrays:
+D=zeros(size(C));   % depth array
+c=C(:)';            % (flattened) location array
+z=zeros(size(c));   % (flattened) value array
+I=1:numel(z);       % (flattened) index array   
 
-for iter=1:max_depth
-    z=z.^2+C;                   % apply iteration of mandelbrot sequence
-    escaped = abs(z) > 2;       % if true, the point has diverged
-    index=find(escaped==1);
-    depth(index) = iter;        % stores number of iterations each point goes through; 0 is included in mandelbrot 
-    escaped = abs(z) > 2;               % if true, the point has diverged
-    depth(escaped) = iter;   % stores number of iterations each point goes through; 0 is included in mandelbrot 
-end 
-figure
-surf(depth,'EdgeColor','None')
-view(2)
+% evolve values:
+for iter=1:max_depth    
+    z=z.^2+c;           % iterate function
+    I_esc=abs(z)>2;     % logically index escaped values  
+    D(I(I_esc))=iter;   % set depth of escaped values
+    % remove escaped elements:
+    I(I_esc)=[];
+    z(I_esc)=[];
+    c(I_esc)=[];
+    fprintf('Depth: %d, surviving points: %d\n',[iter,numel(I)])
+end
+
+% plot Mandelbrot set:
+figure; imagesc(D); axis image; axis off; colormap hot
+
+toc
 
 
 
